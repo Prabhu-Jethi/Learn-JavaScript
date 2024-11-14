16b2f3369eec15fcd5bc6448c1ed31c33ec99ea6
Prabhu <167219745+Prabhu-Jethi@users.noreply.github.com>
2024-11-15 02:41:43 +0530

    Add files via upload

---
+++ 1_basics/dateandtime.js
@@ -0,0 +1,28 @@
+// Date and Time in JS //
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let dt = new Date(2024, 9, 14)
+// let dt = new Date("06-29-2024")
+// console.log(dt.toLocaleDateString());
+
+// let timestamp = Date.now()
+// console.log(timestamp);
+// console.log(dt.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",
+// })
